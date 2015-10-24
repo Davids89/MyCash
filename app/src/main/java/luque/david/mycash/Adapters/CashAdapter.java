5a4c8a6d0f8de86078da9e6f3b88f1c8ae4e027e
David Luque <daviluqui@gmail.com>
2015-10-24 19:11:47 +0200

    Added RecyclerView :)

---
+++ app/src/main/java/luque/david/mycash/Adapters/CashAdapter.java
@@ -0,0 +1,52 @@
+package luque.david.mycash.Adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import luque.david.mycash.Models.Cash;
+import luque.david.mycash.R;
+
+/**
+ * Created by David on 24/10/15.
+ */
+public class CashAdapter extends RecyclerView.Adapter<CashAdapter.CashAdapterHolder> {
+
+    private List<Cash> mList;
+
+    public static class CashAdapterHolder extends RecyclerView.ViewHolder{
+
+        public TextView value;
+
+        public CashAdapterHolder(View v){
+            super(v);
+            value = (TextView) v.findViewById(R.id.valueCard);
+        }
+    }
+
+    public CashAdapter(List<Cash> items){
+        this.mList = items;
+    }
+
+    @Override
+    public CashAdapterHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cash_card, parent, false);
+
+        return new CashAdapterHolder(v);
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.mList.size();
+    }
+
+    @Override
+    public void onBindViewHolder(CashAdapter.CashAdapterHolder holder, int position) {
+        holder.value.setText(mList.get(position).getValue().toString());
+    }
+}
