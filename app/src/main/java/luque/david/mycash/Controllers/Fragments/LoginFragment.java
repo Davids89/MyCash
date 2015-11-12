76c5e015d7673bb08904513fd2f8d05476234c21
David Luque <daviluqui@gmail.com>
2015-11-12 16:29:24 +0100

    Get user info from parse currentUser

--- app/src/main/java/luque/david/mycash/Controllers/Fragments/LoginFragment.java
+++ app/src/main/java/luque/david/mycash/Controllers/Fragments/LoginFragment.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.widget.CardView;
@@ -29,6 +30,8 @@ public class LoginFragment extends Fragment {
     TextInputLayout username;
     TextInputLayout password;
     CardView login;
+
+    public static final String spFile = "userPrefs";
 
 
     public LoginFragment() {
@@ -55,23 +58,6 @@ public class LoginFragment extends Fragment {
                             @Override
                             public void done(ParseUser user, ParseException e) {
                                 if (e == null) {
-
-                                    ParseUser userdata = ParseUser.getCurrentUser();
-
-                                    String userEmail = userdata.getEmail();
-                                    String name = userdata.getString("username");
-                                    String birthdate = userdata.getString("birthdate");
-                                    String location = userdata.getString("location");
-
-                                    Log.d("DATOS ANTES", userEmail + " " + name + " " + birthdate + " " + location);
-
-                                    SharedPreferences myUserData = getActivity().getPreferences(Context.MODE_PRIVATE);
-                                    SharedPreferences.Editor editor = myUserData.edit();
-                                    editor.putString(userEmail, "email");
-                                    editor.putString(name, "name");
-                                    editor.putString(birthdate, "birthdate");
-                                    editor.putString(location, "location");
-                                    editor.commit();
 
                                     Intent myIntent = new Intent(getActivity(), MainActivity.class);
                                     startActivity(myIntent);
--- app/src/main/java/luque/david/mycash/Controllers/Fragments/ProfileFragment.java
+++ app/src/main/java/luque/david/mycash/Controllers/Fragments/ProfileFragment.java
@@ -5,10 +5,13 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.parse.ParseUser;
 
 import luque.david.mycash.R;
 
@@ -16,6 +19,8 @@ import luque.david.mycash.R;
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
+
+    public static final String spFile = "userPrefs";
 
 
     public ProfileFragment() {
@@ -29,13 +34,12 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        SharedPreferences myUserData = getActivity().getPreferences(Context.MODE_PRIVATE);
-        String email = myUserData.getString("email", "");
-        String username = myUserData.getString("username", "Nombre");
-        String birthdate = myUserData.getString("birthdate", "Sin datos");
-        String location = myUserData.getString("location", "Sin datos");
+        ParseUser currentUser = ParseUser.getCurrentUser();
 
-        Log.d("Datos", email + " " + username + " " + birthdate + " " + location);
+        String name = currentUser.getString("username");
+        String email = currentUser.getString("email");
+        String birthdate = currentUser.getString("birthdate");
+        String locatin = currentUser.getString("location");
 
         return rootView;
     }
