01544f876fe998d303a7b8321e425698d0d97e54
David Luque <daviluqui@gmail.com>
2015-11-13 12:42:15 +0100

    Show user data

--- app/src/main/java/luque/david/mycash/Controllers/Fragments/ProfileFragment.java
+++ app/src/main/java/luque/david/mycash/Controllers/Fragments/ProfileFragment.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.parse.ParseUser;
 
@@ -21,6 +22,10 @@ import luque.david.mycash.R;
 public class ProfileFragment extends Fragment {
 
     public static final String spFile = "userPrefs";
+    TextView name_profile;
+    TextView email_profile;
+    TextView birthdate_profile;
+    TextView location_profile;
 
 
     public ProfileFragment() {
@@ -36,12 +41,47 @@ public class ProfileFragment extends Fragment {
 
         ParseUser currentUser = ParseUser.getCurrentUser();
 
+        name_profile = (TextView) rootView.findViewById(R.id.name_profile);
+        email_profile = (TextView) rootView.findViewById(R.id.email_profile);
+        birthdate_profile = (TextView) rootView.findViewById(R.id.birthdate_profile);
+        location_profile = (TextView) rootView.findViewById(R.id.location_profile);
+
         String name = currentUser.getString("username");
         String email = currentUser.getString("email");
         String birthdate = currentUser.getString("birthdate");
-        String locatin = currentUser.getString("location");
+        String location = currentUser.getString("location");
+
+        initValues(name, email, birthdate, location);
 
         return rootView;
+    }
+
+    public void initValues(String name, String email, String birthdate, String location){
+
+        if(name != null){
+            name_profile.setText(name);
+        }else{
+            name_profile.setText("No has introducido tu nombre");
+        }
+
+        if(email != null){
+            email_profile.setText(email);
+        }else{
+            email_profile.setText("usuario@correo.com");
+        }
+
+        if(birthdate != null){
+            birthdate_profile.setText(birthdate);
+        }else{
+            birthdate_profile.setText("01/01/2015");
+        }
+
+        if(location != null) {
+            location_profile.setText(location);
+        }else{
+            //TODO coger la localizacion por gps
+            location_profile.setText("Sin localización");
+        }
     }
 
 
--- app/src/main/java/luque/david/mycash/Models/User.java
+++
@@ -1,8 +0,0 @@
-package luque.david.mycash.Models;
-
-/**
- * Created by David on 7/11/15.
- */
-public class User {
-
-}
